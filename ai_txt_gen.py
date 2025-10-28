23256ae843cdfffedfb693af00e8b90fa91ff409
Nadya Ershova <nadezhda18@gmail.com>
2025-10-28 17:43:34 -0400

    Fixed code based on the changes in the other libs and added missing self.

--- ai_txt_gen.py
+++ ai_txt_gen.py
@@ -24,6 +24,12 @@ class PostWriter:
         self.test = test
         self.callback = callback
 
+    def __get_verbosity_by_topic__(self, post_topic: str) -> int:
+        if self.post_topic == POST_TOPIC_RECIPES:
+            return CHATGPT_VERBOSITY_HIGH
+        else:
+            return CHATGPT_VERBOSITY_MEDIUM
+
     def write_post(self):
         self.post_title = self.post_title.strip()
         self.post_topic = self.post_topic.strip()
@@ -33,38 +39,28 @@ class PostWriter:
         if self.test:
             return f"{self.post_title} not modified", f"Test post about {self.post_title} of type {self.post_topic}"
 
-        SYS_PROMPT_BASE = f"yOU ARE A PROFESSIONAL {self.post_topic} WRITER AND COPYWRITER.{AI_TXT_SYS_PROMPT_STYLE_BY_TOPIC[self.post_topic]}  You write in a clear and concise manner, making complex topics easy to understand. You have a knack for storytelling and can weave narratives that captivate readers.You are also skilled at SEO writing, ensuring that your content is optimized for search engines while still being enjoyable to read."
+        SYS_PROMPT_BASE = f"yOU ARE A PROFESSIONAL {self.post_topic} WRITER AND COPYWRITER.{self.AI_TXT_SYS_PROMPT_STYLE_BY_TOPIC[self.post_topic]}  You write in a clear and concise manner, making complex topics easy to understand. You have a knack for storytelling and can weave narratives that captivate readers.You are also skilled at SEO writing, ensuring that your content is optimized for search engines while still being enjoyable to read."
 
-        sys_prompt = SYS_PROMPT_BASE
-        sys_prompt += AI_TXT_GEN_PROMPTS_BY_TOPIC[self.post_topic]["post"]
-
-        user_prompt = f"Write a detailed {self.post_topic} blog post about '{self.post_title}'. Make sure to follow the structure and style guidelines provided. The post should be engaging, informative, and easy to read. Ensure the content is original and provides value to the readers."
-
-        verbosity = CHATGPT_VERBOSITY_HIGH if self.post_topic == KOALA_POST_TYPE_RECIPE else CHATGPT_VERBOSITY_MEDIUM
-
-        post_txt = send_prompt_to_openai(
-            system_prompt=sys_prompt,
-            user_prompt=user_prompt,
+        prompt_config = AIPromptConfig(
+            system_prompt="",
+            user_prompt="",
             response_format="",
             ai_model=CHATGPT_MODEL,
-            verbosity=verbosity,
-            test=False)
+            verbosity=self.__get_verbosity_by_topic__(self.post_topic)
+        )
+
+        prompt_config.system_prompt = SYS_PROMPT_BASE + self.AI_TXT_GEN_PROMPTS_BY_TOPIC[self.post_topic]["post"]
+        prompt_config.user_prompt = f"Write a detailed {self.post_topic} blog post about '{self.post_title}'. Make sure to follow the structure and style guidelines provided. The post should be engaging, informative, and easy to read. Ensure the content is original and provides value to the readers."
+
+        post_txt = send_prompt_to_openai(prompt_config, self.test)
 
         if post_txt["error"] != "":
             raise OpenAIAPIError(f"OpenAI API error: {post_txt['error']} '{post_txt['message']}'")
 
-        sys_prompt = SYS_PROMPT_BASE
-        sys_prompt += AI_TXT_GEN_PROMPTS_BY_TOPIC[self.post_topic]["title"]
+        prompt_config.system_prompt = SYS_PROMPT_BASE + self.AI_TXT_GEN_PROMPTS_BY_TOPIC[self.post_topic]["title"]
+        prompt_config.user_prompt = f"Generate a catchy and SEO-friendly blog post title for the following blog post about '{self.post_title}'. The title should be engaging and encourage readers to click on the article. It should also include relevant keywords that would help improve the post's search engine ranking.\nPost text:\n{post_txt['message']}"
 
-        user_prompt = f"Generate a catchy and SEO-friendly blog post title for the following blog post about '{self.post_title}'. The title should be engaging and encourage readers to click on the article. It should also include relevant keywords that would help improve the post's search engine ranking.\nPost text:\n{post_txt['message']}"
-
-        post_title = send_prompt_to_openai(
-            system_prompt=sys_prompt,
-            user_prompt=user_prompt,
-            response_format="",
-            ai_model=CHATGPT_MODEL,
-            verbosity=CHATGPT_VERBOSITY_MEDIUM,
-            test=False)
+        post_title = send_prompt_to_openai(prompt_config, self.test)
 
         if post_title["error"] != "":
             raise OpenAIAPIError(f"OpenAI API error: {post_title['error']} '{post_title['message']}'")
